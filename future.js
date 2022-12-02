ea05fed09854179adc2b08d7da72abda23e36bec
IORD1 <pratham111ingole@gmail.com>
2022-12-02 22:07:26 +0530

    added future js

---
+++ future.js
@@ -0,0 +1,14 @@
+var totalAmount;
+var downPay;
+var months;
+
+
+
+totalAmount = 1000;
+downPay = 200;
+months = 5;
+function calculate(){
+    console.log((totalAmount-downPay)/months);
+}   
+
+calculate(); 
